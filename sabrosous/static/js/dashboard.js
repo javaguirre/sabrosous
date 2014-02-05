154b53e2d85f001d8e94a5fc1a6405acf3966ffc
Javier Aguirre <contacto@javaguirre.net>
2014-02-05 16:44:14 +0100

    Adding and deleting buttons

--- sabrosous/static/js/dashboard.js
+++ sabrosous/static/js/dashboard.js
@@ -1,17 +1,38 @@
 /** @jsx React.DOM */
 var Link = React.createClass({
+    deleteObj: function() {
+        this.props.onDelete(this.props.key);
+    },
+    editObj: function() {
+        console.log('edit');
+    },
     render: function() {
         var pub_date = moment(this.props.pub_date).calendar();
-        var domain = parseUri(this.props.url).host;
+        var uri_obj = parseUri(this.props.url);
+        var domain = uri_obj.host;
+        var domain_url = uri_obj.protocol + '://' + domain;
 
         return (
-            <div className="row link-block well">
-                <div>
-                    <a href={this.props.url} className="link-title">{this.props.title}</a>
-                    <a href={domain} className="link-title">{domain}</a>
+            <div ref="item" data-id={this.props.id}
+                 className="row link-block well">
+                <div className="col-md-11">
+                    <div>
+                        <a href={this.props.url} className="link-title">{this.props.title}</a>
+                        <a href={domain_url} className="link-title">{domain}</a>
+                    </div>
+                    <div>
+                        {this.props.tags} <span className="pull-right">{pub_date}</span>
+                    </div>
                 </div>
-                <div>
-                    {this.props.tags} <span className="pull-right">{pub_date}</span>
+                <div className="col-md-1">
+                    <div>
+                        <a href="#edit" onClick={this.editObj}>
+                            <span className="glyphicon glyphicon-pencil edit-obj"></span>
+                        </a>
+                        <a href="#delete" onClick={this.deleteObj}>
+                            <span className="glyphicon glyphicon-remove delete-obj"></span>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
@@ -26,17 +47,52 @@ var Tag = React.createClass({
             <a href={hash} className="tag small">{this.props.name}</a>
         )
     }
-})
+});
+
+var LinkForm = React.createClass({
+    getInitialState: function() {
+        return {add: 'none'};
+    },
+    toggleAdd: function() {
+        var add = 'none';
+
+        if(this.state.add == 'none') {
+            add = 'block';
+        }
+
+        this.setState({add: add});
+    },
+
+    addObj: function() {
+        var url = this.refs.url.getDOMNode().value.trim();
+        this.props.onSubmit(url);
+    },
+
+    render: function() {
+        return (
+            <div className="link-add">
+                <a href="#add" onClick={this.toggleAdd}>Add</a>
+
+                <div style={{display: this.state.add}}>
+                    <input type="text" ref="url" />
+                    <input type="button" onClick={this.addObj} value="add" />
+                </div>
+            </div>
+        )
+    }
+});
 
 var LinkList = React.createClass({
     render: function() {
+        var onDelete = this.props.onDelete;
         var links = this.props.data.map(function(link) {
             var tags = link.tags.map(function(tag) {
-                return <Tag slug={tag.slug} name={tag.name} />;
+                return <Tag slug={tag.slug} name={tag.name} key={tag.id} />;
             });
 
             return <Link url={link.url} title={link.title} key={link.id}
                          tags={tags} pub_date={link.pub_date}
+                         onDelete={onDelete}
                          />;
         });
 
@@ -45,18 +101,6 @@ var LinkList = React.createClass({
                 {links}
             </div>
         )
-    }
-});
-
-var LinkForm = React.createClass({
-    getInitialState: function() {
-        return {edit: 'none'};
-    },
-    render: function() {
-        return (
-            <div>
-            </div>
-        );
     }
 });
 
@@ -76,11 +120,8 @@ var LinkImport = React.createClass({
             contentType: false,
             processData: false,
             data: data,
-            headers: {
-                'X-CSRFToken': $.cookie('csrftoken')},
-            success: function(data) {
-                console.log('Goed!');
-            }
+            headers: {'X-CSRFToken': $.cookie('csrftoken')},
+            success: function(data) {}
         });
         return false;
     },
@@ -110,6 +151,37 @@ var LinkImport = React.createClass({
 });
 
 var LinkBox = React.createClass({
+    handleAddLink: function(url) {
+        var links = this.state.data;
+
+        data = {url: url};
+
+        $.ajax({
+            type: 'POST',
+            url: this.props.url,
+            data: data,
+            headers: {'X-CSRFToken': $.cookie('csrftoken')},
+            success: function(data) {
+                var newLinks = links.concat([data]);
+                this.setState(newLinks);
+            }.bind(this)
+        });
+    },
+    deleteObj: function(data_id) {
+        var links = this.state.data;
+        var newlinks = links.filter(function(elem) {
+            return elem.id != data_id;
+        });
+
+        this.setState({data: newlinks});
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/links/' + data_id + '/',
+            success: function() {}.bind(this),
+            headers: {'X-CSRFToken': $.cookie('csrftoken')}
+        });
+    },
     search: function(query) {
         if(query === "") {
             return this;
@@ -143,7 +215,10 @@ var LinkBox = React.createClass({
             <div>
                 <h3>Links</h3>
                 <LinkImport />
-                <LinkList data={this.state.data} />
+
+                <LinkForm url={this.props.url} onSubmit={this.handleAddLink} />
+
+                <LinkList data={this.state.data} onDelete={this.deleteObj} />
             </div>
         )
     }
